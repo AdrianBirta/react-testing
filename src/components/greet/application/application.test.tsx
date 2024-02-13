49eb12fbb984c5a799c49aa5915ccbb96980419e
Adrian Birta <adrianbirta@gmail.com>
2024-02-13 13:32:35 +0200

    Add getByRole method

--- src/components/greet/application/application.test.tsx
+++ src/components/greet/application/application.test.tsx
@@ -4,7 +4,34 @@ import { render, screen } from '@testing-library/react';
 describe('Application', () => {
   test('renders correctly', () => {
     render(<Application />);
-    const nameElement = screen.getByRole('textbox'); // element
+
+    const pageHeading = screen.getByRole('heading', {
+      name: 'Job application form',
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole('heading', {
+      level: 2, // 2 for h2 - h{level}
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole('textbox', { name: 'Name' }); // element
     expect(nameElement).toBeInTheDocument(); // assection
+
+    const bioElement = screen.getByRole('textbox', {
+      name: 'Bio',
+    });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole('combobox');
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole('checkbox');
+    expect(termsElement).toBeInTheDocument();
+
+    const submitButtonElement = screen.getByRole('button');
+    expect(submitButtonElement).toBeInTheDocument();
   });
 });
+
+// https://www.w3.org/TR/html-aria/#docconformance
