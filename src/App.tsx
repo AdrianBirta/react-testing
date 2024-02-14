9842079086d63738ac64f7865202f60b1015b584
Adrian Birta <adrianbirta@gmail.com>
2024-02-14 16:29:23 +0200

    Add pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { Application } from './components/application/application';
+import { Counter } from './components/counter/counter';
 import { Skills } from './components/skills/skills';
 
 function App() {
   return (
     <div className="App">
-      <Skills skills={['HTML', 'CSS']} />
+      {/* <Application />
+      <Skills skills={['HTML', 'CSS']} /> */}
+      <Counter />
     </div>
   );
 }
