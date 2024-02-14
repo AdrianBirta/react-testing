fa772dd0cd0cb9236eb36326c4a55b17e114384f
Adrian Birta <adrianbirta@gmail.com>
2024-02-14 09:03:48 +0200

    Add the query for multiple elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Application } from './components/greet/application/application';
+import { Application } from './components/application/application';
 
 function App() {
   return (
