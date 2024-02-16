118f969d96065b0b0c54a5331a2b6a655c71954c
Adrian Birta <adrianbirta@gmail.com>
2024-02-16 14:27:12 +0200

    Add providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css';
 import { Application } from './components/application/application';
 import { Counter } from './components/counter/counter';
 import { Skills } from './components/skills/skills';
+import { AppProviders } from './providers/app-providers';
+import { MuiMode } from './components/mui/mui-mode';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Application />
-      <Skills skills={['HTML', 'CSS']} /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
